6e8af1c61e13ba513449048329360b23169d7948
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-09-10 19:50:45 -0300

    ✨ (feat): Tickets JPARepository

--- src/main/java/com/pardal/app/repository/TicketRepository.java
+++ src/main/java/com/pardal/app/repository/TicketRepository.java
@@ -1,7 +1,10 @@
 package com.pardal.app.repository;
 
 import com.pardal.app.entity.Tickets;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface TicketRepository extends Repository<Tickets, Long> {
+public interface TicketRepository extends JpaRepository<Tickets, Long>, JpaSpecificationExecutor<Tickets> {
+
+
 }
