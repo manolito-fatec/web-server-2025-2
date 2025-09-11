0a16241bc16d66f872c80ec8c98306a179f6c536
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-09-11 16:51:53 -0300

    ✨(feat): added query to get tickets

--- src/main/java/com/pardal/app/repository/TicketRepository.java
+++ src/main/java/com/pardal/app/repository/TicketRepository.java
@@ -1,7 +1,17 @@
 package com.pardal.app.repository;
 
 import com.pardal.app.entity.Tickets;
-import org.springframework.data.repository.Repository;
 
-public interface TicketRepository extends Repository<Tickets, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Tickets, Long> {
+
+    @Query("SELECT t FROM Tickets t WHERE t.productId = :productId")
+    List<Tickets> findByProductId(@Param("productId") Integer productId);
 }
