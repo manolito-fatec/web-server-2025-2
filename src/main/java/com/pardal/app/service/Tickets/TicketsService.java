21deac847ec105fb200246bec01ccb98c3305547
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-09-13 23:18:22 -0300

    ✨(feat): added method to get tickets by product

--- src/main/java/com/pardal/app/service/Tickets/TicketsService.java
+++ src/main/java/com/pardal/app/service/Tickets/TicketsService.java
@@ -1,9 +1,12 @@
 package com.pardal.app.service.Tickets;
 
+import com.pardal.app.entity.Dto.TicketsByProductsCountDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface TicketsService {
     long getTicketsCount(Optional<Integer> productId, Optional<Integer> clientId, Optional<LocalDateTime> dateMin, Optional<LocalDateTime> dateMax);
+    List<TicketsByProductsCountDto> getTicketsCountGroupedByProduct();
 }
