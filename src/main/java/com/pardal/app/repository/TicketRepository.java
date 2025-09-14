f094ef6df915028b36264ec3b55b5791cd3e15a9
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-09-13 23:17:07 -0300

    ✨(feat): query to get tickets by product

--- src/main/java/com/pardal/app/repository/TicketRepository.java
+++ src/main/java/com/pardal/app/repository/TicketRepository.java
@@ -1,10 +1,16 @@
 package com.pardal.app.repository;
 
+import com.pardal.app.entity.Dto.TicketsByProductsCountDto;
 import com.pardal.app.entity.Tickets;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Tickets, Long>, JpaSpecificationExecutor<Tickets> {
+    @Query("SELECT new com.pardal.app.entity.Dto.TicketsByProductsCountDto(t.product.id, COUNT(t)) FROM Tickets t GROUP BY t.product.id")
+    List<TicketsByProductsCountDto> getTicketsCountGroupedByProduct();
 }
