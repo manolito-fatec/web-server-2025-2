2c9ff2e73215708e3ac25cd838b58cd48dc70d11
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-09-15 20:12:57 -0300

    ✨(feat): added news responses to TicketsController
    also added swagger

--- src/main/java/com/pardal/app/controllers/TicketsController.java
+++ src/main/java/com/pardal/app/controllers/TicketsController.java
@@ -2,6 +2,9 @@ package com.pardal.app.controllers;
 
 import com.pardal.app.entity.Dto.TicketsByProductsCountDto;
 import com.pardal.app.service.Tickets.TicketsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +21,24 @@ public class TicketsController {
 
     private final TicketsService ticketsService;
 
+    @Operation(summary = "Busca a contagem de tickets agrupados por produto", description = "Retorna uma lista com a contagem total de tickets para cada produto.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contagem de tickets extraída com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Nenhum ticket ou produto encontrado."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor ao tentar buscar a contagem de tickets.")
+    })
     @GetMapping("/by-product")
-    public ResponseEntity<List<TicketsByProductsCountDto>> getTicketsByProduct() {
+    public ResponseEntity<?> getTicketsByProduct() {
         try {
-            return ResponseEntity.ok(ticketsService.getTicketsCountGroupedByProduct());
+            List<TicketsByProductsCountDto> result = ticketsService.getTicketsCountGroupedByProduct();
+            if (result.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Found no tickets");
+            }
+            return ResponseEntity.ok(result);
         } catch (NoSuchElementException noSuchElementException) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Found no tickets");
+        } catch (RuntimeException runtimeException) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(runtimeException.getMessage());
         }
     }
 }
