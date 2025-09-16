159a23a6d47f3bb7face200ffb11ea33b8baa0c5
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-09-16 20:02:57 -0300

    ✨(feat): method to get all closed ticket dates

--- src/main/java/com/pardal/app/repository/TicketRepository.java
+++ src/main/java/com/pardal/app/repository/TicketRepository.java
@@ -13,4 +13,5 @@ import java.util.List;
 public interface TicketRepository extends JpaRepository<Tickets, Long>, JpaSpecificationExecutor<Tickets> {
     @Query("SELECT new com.pardal.app.entity.Dto.TicketsByProductsCountDto(t.product.id, COUNT(t)) FROM Tickets t GROUP BY t.product.id")
     List<TicketsByProductsCountDto> getTicketsCountGroupedByProduct();
+    List<Tickets> findAllByClosedAtIsNotNull();
 }
