238a7a82aac4df55b0e52ccbb510e711b479f309
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2025-09-26 12:10:47 -0300

    :construction_worker:(ci): test sonar cloud

--- src/main/java/com/pardal/app/service/metrics/MetricsServiceImpl.java
+++ src/main/java/com/pardal/app/service/metrics/MetricsServiceImpl.java
@@ -3,6 +3,7 @@ package com.pardal.app.service.metrics;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Optional;
 
 import com.pardal.app.entity.Tickets;
