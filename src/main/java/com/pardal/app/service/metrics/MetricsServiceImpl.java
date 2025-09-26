fe445051d5beb184b3a966ee2848bab4c14b34bc
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2025-09-26 12:44:15 -0300

    :construction_worker:(ci): remove test sonar and remove main branch in sonar build

--- src/main/java/com/pardal/app/service/metrics/MetricsServiceImpl.java
+++ src/main/java/com/pardal/app/service/metrics/MetricsServiceImpl.java
@@ -3,7 +3,6 @@ package com.pardal.app.service.metrics;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Optional;
-import java.util.Set;
 
 import com.pardal.app.entity.Tickets;
 import com.pardal.app.entity.dto.DashboardFilterDto;
