6e13a3ec0eaf3c78b7e3fb5a6c1ee5b1b329bf8d
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2025-10-08 23:51:15 -0300

    :recycle:(refactor): Removed unnecessary test

--- src/test/java/com/pardal/app/service/tickets/TicketsServiceImplTest.java
+++ src/test/java/com/pardal/app/service/tickets/TicketsServiceImplTest.java
@@ -5,8 +5,7 @@ import com.pardal.app.entity.dto.DashboardFilterDto;
 import com.pardal.app.entity.dto.TicketCountDto;
 import com.pardal.app.enums.GroupingPeriods;
 import com.pardal.app.repository.TicketRepository;
-import com.pardal.app.repository.specification.MetricsSpecifications;
-import com.pardal.app.util.Gambiarra;
+import com.pardal.app.repository.specification.TicketsSpecification;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -29,10 +29,6 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TicketsServiceImplTest {
-
-    @Mock
-    @Gambiarra(descricao = "injetado para evitar null pointers nos testes. o ideal é Specifications ter métodos estáticos", autor = "AndreWakugawa")
-    private MetricsSpecifications metricsSpecifications;
 
     @Mock
     private TicketRepository ticketRepository;
