799b01041517f508c9db8547f78de31d04128963
CaueVSilv <cauevsilva2002@gmail.com>
2025-10-11 16:55:07 +0000

    ♻️ (refactor): renamed app_profile table

--- src/main/resources/db/migration/V2__appuser_creation.sql
+++ src/main/resources/db/migration/V2__appuser_creation.sql
@@ -1,22 +1,23 @@
-CREATE TABLE app_profile (
-    prf_id serial,
-    prf_name VARCHAR(20) NOT NULL,
+CREATE TABLE app_role (
+    rl_id serial,
+    rl_name VARCHAR(20) NOT NULL,
 
-    CONSTRAINT prf_id_pk primary key (prf_id)
+    CONSTRAINT rl_id_pk primary key (rl_id)
 );
 
-INSERT INTO app_profile (prf_name) values ('Admin');
+INSERT INTO app_role (rl_name) values ('Admin');
 
 CREATE TABLE app_users (
                            usr_id serial,
                            usr_name VARCHAR(255) NOT NULL,
-                           usr_email VARCHAR(255) NOT NULL,
+                           usr_email VARCHAR(255) UNIQUE NOT NULL,
                            usr_phone VARCHAR(15) NOT NULL,
-                           prf_id int NOT NULL DEFAULT 1,
+                           rl_id int NOT NULL DEFAULT 1,
                            usr_expire_date date DEFAULT NULL,
+                           usr_pwd VARCHAR(255) ,
 
                            CONSTRAINT usr_id_pk primary key (usr_id),
-                           CONSTRAINT prf_id_fk FOREIGN KEY (prf_id)
-                               REFERENCES app_profile(prf_id)
+                           CONSTRAINT rl_id_fk FOREIGN KEY (rl_id)
+                               REFERENCES app_role(rl_id)
 
 );
