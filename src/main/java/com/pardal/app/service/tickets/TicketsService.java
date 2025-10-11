20fdd137b1a57474edd613bbc1759c58f19f06b4
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-10-11 14:33:43 -0300

    🔀(merge): feat/id87/count subcategory

--- src/main/java/com/pardal/app/service/tickets/TicketsService.java
+++ src/main/java/com/pardal/app/service/tickets/TicketsService.java
@@ -4,11 +4,12 @@ import com.pardal.app.entity.Tickets;
 import com.pardal.app.entity.dto.DashboardFilterDto;
 import com.pardal.app.entity.dto.TicketCountDto;
 import com.pardal.app.entity.dto.TicketsByProductsCountDto;
-
+import com.pardal.app.entity.dto.TicketsBySubcategoryCountDto;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface TicketsService {
     long getTicketsCount(DashboardFilterDto filters);
@@ -16,7 +17,7 @@ public interface TicketsService {
     List<TicketsByProductsCountDto> getTicketsCountGroupedByProduct(Specification<Tickets> baseSpec);
 
     double getSlaCompliantPercentage(Specification<Tickets> baseSpec);
-
+  
     Double getAverageTicketClosureTimeInHours(Specification<Tickets> baseSpec);
 
     List<TicketCountDto> getTicketCountByPeriod(DashboardFilterDto filters);
@@ -24,4 +25,6 @@ public interface TicketsService {
     Long getAllTicketsCount(Specification<Tickets> baseSpec);
 
     BigDecimal getReopenedTicket(DashboardFilterDto pFilters);
+
+    List<TicketsBySubcategoryCountDto> getCountSubcategory(Specification<Tickets> baseSpec);
 }
