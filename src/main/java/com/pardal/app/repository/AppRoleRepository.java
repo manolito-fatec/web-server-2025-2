f6b4f005463a9d7ea6add865b7b46b8b94a4bed0
CauevSilv <caue.silva10@fatec.sp.gov.br>
2025-10-13 22:00:23 -0300

    🐛 (fix): repository types

--- src/main/java/com/pardal/app/repository/AppRoleRepository.java
+++ src/main/java/com/pardal/app/repository/AppRoleRepository.java
@@ -1,6 +1,7 @@
 package com.pardal.app.repository;
 
 import com.pardal.app.entity.AppRole;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
 
@@ -8,4 +9,6 @@ import java.util.Set;
 
 public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
     Set<AppRole> findById(Integer id);
+
+    @NotNull AppRole getAppRoleById(Integer id);
 }
--- src/main/java/com/pardal/app/repository/AppUserRepository.java
+++ src/main/java/com/pardal/app/repository/AppUserRepository.java
@@ -1,11 +1,11 @@
 package com.pardal.app.repository;
 
 import com.pardal.app.entity.AppUser;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AppUserRepository extends Repository<AppUser, Long> {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 
     Optional<AppUser> findById(Integer id);
