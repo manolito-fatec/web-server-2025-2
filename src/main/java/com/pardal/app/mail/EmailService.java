94acde8d486b8117271984dceaa0ec4f5267b717
CaueVSilv <cauevsilva2002@gmail.com>
2025-10-16 00:52:40 +0000

    ✨ (feat): add email HTML template

--- src/main/java/com/pardal/app/mail/EmailService.java
+++ src/main/java/com/pardal/app/mail/EmailService.java
@@ -1,13 +1,18 @@
 package com.pardal.app.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
-
+@Service
 public class EmailService {
 
-
+    @Autowired
     private JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
@@ -17,17 +22,33 @@ public class EmailService {
         String baseUrl = "https://localhost:8080";
         String verificationUrl = baseUrl + "/api/auth/verify?token=" + token;
 
-        String subject = "Confirmação de Cadastro";
-        String content = "Olá, clique no link abaixo para confirmar seu e-mail:\n" + verificationUrl;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setText(content);
-        message.setTo("caueherobrine@gmail.com");
-        message.setFrom(sender);
+        String subject = "Confirmação do seu cadastro na plataforma Pardal!";
+
+        String htmlContent = """
+    <html>
+        <body>
+            <img src="cid:logo" alt="Logo" style="width: 150px; height: auto;">
+            <h1>Bem-vindo à plataforma Pardal!</h1>
+            <p>Olá, clique <a href="%s">aqui</a> para confirmar seu e-mail.</p>
+        </body>
+    </html>
+    """.formatted(verificationUrl);
 
         try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true);
+            helper.setTo(recipient);
+            helper.setFrom(sender);
+
+            ClassPathResource image = new ClassPathResource("images/logo.png");
+            helper.addInline("logo", image);
+
             mailSender.send(message);
             return "Email sent successfully!";
-        } catch (Exception e) {
+        } catch (MessagingException e) {
             e.printStackTrace();
             return "Error sending email.";
         }
