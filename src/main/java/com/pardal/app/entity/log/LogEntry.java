f288b9772c38ae3aebd29ed1294724e6d2511ac9
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2025-11-09 18:06:13 -0300

    :sparkles:(feat): added new entity log

---
+++ src/main/java/com/pardal/app/entity/log/LogEntry.java
@@ -0,0 +1,30 @@
+package com.pardal.app.entity.log;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "application_logs")
+public class LogEntry
+{
+    @Id
+    private String id;
+    private String level;
+    private String logger;
+    private String message;
+    private LocalDateTime timestamp;
+    private String thread;
+    private String exception;
+    private String httpMethod;
+    private String requestURI;
+}
