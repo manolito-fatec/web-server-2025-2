6f521404739e803b1f1ddf6b0240b8f72e610d0c
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-11-15 11:53:04 -0300

    ✨ (feat): add Dek findAll method

--- src/main/java/com/pardal/app/service/dek/DekService.java
+++ src/main/java/com/pardal/app/service/dek/DekService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,6 +31,10 @@ public class DekService {
             throw new RuntimeException("Erro ao excluir DataEncryptionKey");
         }
         return true;
+    }
+
+    public List<DataEncryptionKey> findAll() {
+        return repository.findAll();
     }
 
     public DataEncryptionKey updateDek(DataEncryptionKey dataEncryptionKey) {
